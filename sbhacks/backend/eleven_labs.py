a4feaf7f40c566e0590c4685921bac42c0e82c4b
Varshini Vijay <varshinivijay05@gmail.com>
2026-01-10 14:59:12 -0800

    final ver

--- sbhacks/backend/eleven_labs.py
+++ sbhacks/backend/eleven_labs.py
@@ -12,6 +12,45 @@ VOICE_IDS = {
     2: "WLOYW6YwyA4c6LBQKJ36",
     3: "l2xKdzGYYWPy0gKbjRXC"
 }
+voice = elevenlabs.text_to_voice.create(
+    voice_name="Berta",
+    voice_description="Middle-aged black lady",
+    # The generated voice ID of the preview you want to use,
+    # using the first in the list for this example
+    generated_voice_id=voice_ids[1]
+)
+
+#l2xKdzGYYWPy0gKbjRXC
+
+for preview in voices.previews:
+    # Convert base64 to audio buffer
+    audio_buffer = base64.b64decode(preview.audio_base_64)
+
+    print(f"Playing preview: {preview.generated_voice_id}")
+
+# voice = elevenlabs.text_to_voice.create(
+#     voice_name="Berta",
+#     voice_description="Middle-aged black lady",
+#     # The generated voice ID of the preview you want to use,
+#     # using the first in the list for this example
+#     generated_voice_id=voice_ids[1]
+# )
+
+# voice = elevenlabs.text_to_voice.create(
+#     voice_name="Andrew",
+#     voice_description="New grad male, deep voice.",
+#     # The generated voice ID of the preview you want to use,
+#     # using the first in the list for this example
+#     generated_voice_id=voice_ids[2]
+# )
+
+# voice = elevenlabs.text_to_voice.create(
+#     voice_name="Sophia",
+#     voice_description="Young, corporate woman",
+#     # The generated voice ID of the preview you want to use,
+#     # using the first in the list for this example
+#     generated_voice_id=voice_ids[3]
+# )
 
 def generate_speech(text, avatar, model_ver=MODEL_VERSION):
     load_dotenv()
