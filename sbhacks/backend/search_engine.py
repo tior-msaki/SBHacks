95e5438aaf4de1e05980f223e0c06bedef321ed8
Varshini Vijay <varshinivijay05@gmail.com>
2026-01-10 18:30:32 -0800

    need to fix how we access the sites

--- sbhacks/backend/search_engine.py
+++ sbhacks/backend/search_engine.py
@@ -1,7 +1,32 @@
 import requests 
-import python_dotenv
+from dotenv import load_dotenv
 import os
 
 load_dotenv()
 GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY") 
 SEARCH_ENGINE_ID = os.getenv("SEARCH_ENGINE_ID")
+
+
+search_query = "neural nine books"
+url = ""f"https://www.googleapis.com/customsearch/v1"
+params = {
+    "key": GOOGLE_API_KEY,
+    "cx": SEARCH_ENGINE_ID,
+    "q": search_query,
+    "num": 5
+}
+
+response = requests.get(url, params=params)
+results = response.json()
+
+for item in results:
+    url = item["items"]["link"]
+    response = requests.get(url, params=params)
+    results = response.json()
+
+
+
+# 200 is the HTTP status code for a successful request
+# if response.status_code == 200:
+#     if 'items' in result 
+    
